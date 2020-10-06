f1c4d066dd6c4a8a723cef2c64545b78a77769c1
troydaniels <troyaadaniels@gmail.com>
2020-10-06 21:37:42 +1100

    Moves dev dependencies to appropriate place in package.json & updates webpack to allow eslint jsx entrypoint

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -285,9 +285,7 @@ module.exports = function (webpackEnv) {
       // https://github.com/facebook/create-react-app/issues/290
       // `web` extension prefixes have been added for better support
       // for React Native Web.
-      extensions: paths.moduleFileExtensions
-        .map((ext) => `.${ext}`)
-        .filter((ext) => useTypeScript || !ext.includes('ts')),
+      extensions: paths.moduleFileExtensions.map(ext => `.${ext}`).filter(ext => useTypeScript || !ext.includes('ts')),
       alias: {
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
@@ -326,7 +324,7 @@ module.exports = function (webpackEnv) {
           use: [
             {
               options: {
-                cache: true,
+                cache: false,
                 formatter: require.resolve('react-dev-utils/eslintFormatter'),
                 eslintPath: require.resolve('eslint'),
                 resolvePluginsRelativeTo: __dirname,
--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from 'react'
 import * as R from 'ramda'
 import 'tachyons'
