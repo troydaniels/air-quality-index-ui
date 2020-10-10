8f914c3f2bcbd66583b26d1274496d10ac024673
troydaniels <troyaadaniels@gmail.com>
2020-10-10 22:16:51 +1100

    Restyles StationList & adds station selection highlighting

--- src/components/StationsList/index.jsx
+++ src/components/StationsList/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as R from 'ramda';
+import classnames from 'classnames';
 import { v4 as uuidv4 } from 'uuid';
 import { useAppState } from '../../state';
 import apiServices from '../../api/apiServices';
@@ -8,14 +9,23 @@ const ERROR_FETCHING_FEED =
   'There was an error retrieving the Air Quality feed from this station. Please check your network connection and try again.';
 
 const StationsList = () => {
-  const { setError, searchResults, setSelection } = useAppState();
-
+  const { setError, searchResults, setSelection, selection } = useAppState();
+  console.log(searchResults);
   return (
-    <div className="flex flex-column w5 ml3">
+    <div
+      className="flex flex-column w5 ml3 overflow-y-auto"
+      style={{ height: '30rem' }}
+    >
       {searchResults ? (
         searchResults.map(({ uid, station: { name } }) => (
           <button
-            className="pv2 ph3 mb1 flex-grow-0 flex-shrink-0 ba b--solid b--light-gray tl w-100 border-box"
+            className={classnames(
+              'pv2 ph3 mb1 flex-grow-0 flex-shrink-0 ba b--solid b--gray tl w-100 border-box',
+              {
+                'bg-lightest-blue': uid !== selection.idx,
+                'bg-light-blue': uid === selection.idx,
+              }
+            )}
             onClick={() =>
               apiServices
                 .getStationFeedByUID(uid)
@@ -32,7 +42,7 @@ const StationsList = () => {
             type="button"
             key={uuidv4()}
           >
-            {name}
+            {name.split(';')[0]}
           </button>
         ))
       ) : (
